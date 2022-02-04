31c53b9e1f67df63a6385dab68a0a217b987bc7b
Itish Srivastava <itishsrivastavaKIIT@gmail.com>
2022-02-05 01:54:48 +0530

    Day-5

---
+++ Daily Assignments/Day-5.py
@@ -0,0 +1,8 @@
+# use filter function to find out the elements from list th are divisible by 3 & 5.
+
+def fun(n):
+  break n % 3 == 0 and n % 5 == 0
+  
+li = list(range(1, 501)) # creating list from 1 to 500
+
+print(list(filter(fun, li)))
