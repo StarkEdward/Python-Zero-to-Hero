a2e9f1b78e5f1aa87c8a65d486a09ddfa291d297
StarkEdward <skamble454@gmail.com>
2022-02-06 12:05:15 +0530

    some changes done to reduce few lines of code by calling Counter function from module collections

--- Daily Assignments/Day-2.py
+++ Daily Assignments/Day-2.py
@@ -1,17 +1,19 @@
 # Lottery Prize Project
+from collections import Counter         # importing Module
 
 x = input("Enter a String: ").upper()
 print("\nWelcome to the Lottery.\n\n\t", x)
 x = x.lower().replace(" ", "")
 ip = input("\nTo win the Lottery.\nChoose the any letter from the above word: ").lower()
 s_ip = x.find(ip[0])    # index number of letter in x
-
+'''
+if you dont want to import Module you can use below method.
 d = {}   # black dictionary
-
 for i in x:
-    d[i] = d.get(i, 0) + 1
+     d[i] = d.get(i, 0) + 1
+'''
+d = dict(Counter(x))
 max_key = max(d, key = d.get)
-
 if ip == max_key:
     print("\nCongratulations!\nYou won the Lottery Prize.")
 else:
